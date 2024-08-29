f61f4f7294ad35fd665ac8f9a44fb386d3f2dbc1
Julian Martinez Gomez <julianmzg@gmail.com>
2024-08-29 09:06:53 -0300

    feat: Add shop pages
    This commit adds the following shop pages:
    - Cart Page
    - Admin Page
    - Empty Page
    - Orders Page
    - Checkout Page
    - Products Page
    - Order Details Page
    - Category Page
    - Product Page
    - Address Page
    
    It also includes the following changes:
    - Added ShopLayout component
    - Updated RootLayout component
    - Added fonts configuration
    
    These changes are necessary to implement the basic structure and functionality of the shop section of the application.

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className={titleFont.className}>Login page</h1> 
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>New account</h1> 
+        </div>
+    );
+}
