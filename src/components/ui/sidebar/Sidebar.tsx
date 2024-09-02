63df4dd1e59ec605449c7c487c710afa718b4b88
Julian Martinez Gomez <julianmzg@gmail.com>
2024-09-02 12:08:22 -0300

    feat: Add lazy loading for images and sidebar component

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5";
+import Link from "next/link";
+
 export const Sidebar = () => {
   return (
     <div>
@@ -9,8 +13,76 @@ export const Sidebar = () => {
 
       {/* Sidemenu*/}
       // todo: slide fx
-      <nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
+      <nav 
+        className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
 
+        <IoCloseOutline size={50} className="absolute top-5 right-5 cursor-pointer" onClick={() => console.log("click")} />
+
+        {/* Input */}
+        <div className="relative mt-14">
+            <IoSearchOutline size={20} className="absolute top-2 left-2" />
+            <input type="text" placeholder="Buscar" className="w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500" />
+        </div>
+
+        {/* Menu */}
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoPersonOutline size={20} />
+            <span className="ml-5">Perfil</span>
+        </Link>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoTicketOutline size={20} />
+            <span className="ml-5">Ordenes</span>
+        </Link>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoLogInOutline size={20} />
+            <span className="ml-5">Ingresar</span>
+        </Link>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoLogOutOutline size={20} />
+            <span className="ml-5">Salir</span>
+        </Link>
+
+        {/* Line Separator */}
+        <div className="w-full h-px bg-gray-200 my-10"></div>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoShirtOutline size={20} />
+            <span className="ml-5">Productos</span>
+        </Link>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoTicketOutline size={20} />
+            <span className="ml-5">Ordenes</span>
+        </Link>
+
+        <Link
+            href={"/"}
+            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        >
+            <IoPeopleOutline size={20} />
+            <span className="ml-5">Usuarios</span>
+        </Link>
 
       </nav>
     </div>
