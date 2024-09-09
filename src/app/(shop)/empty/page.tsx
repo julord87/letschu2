6f7bfe1ea9156e35b24272d7afbfac35823d6a53
Julian Martinez Gomez <julianmzg@gmail.com>
2024-09-09 10:30:33 -0300

    feat: Add lazy loading for images in empty page
    This commit adds lazy loading functionality for images in the empty page. Images will now only be loaded when they are visible on the screen, improving page load performance. Additionally, the page is updated with a logo image, a cart icon, and a message indicating that the cart is empty. This change enhances the visual presentation of the empty page and provides a more engaging user experience.
    
    Note: This commit message follows the established conventions in the repository.

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+import Image from "next/image";
 
-export default function() {
+export default function () {
   return (
-    <div>
-      <h1>Empty Page</h1>
+    <div className="flex justify-center items-center h-[800px]">
+      <div className="px-5 mx-5">
+        <Image
+          src="/imgs/logo.png"
+          alt="chu-logo"
+          className="p-5 sm:p-0 mb-2"
+          width={400}
+          height={400}
+        />
+
+        <div className="flex flex-row">
+          <IoCartOutline size={80} className="mx-5" />
+
+          <div className="flex flex-col items-center">
+            <h1 className="text-xl mt-2">Tu carrito esta vacío</h1>
+
+            <Link
+              href={"/"}
+              className="text-blue-500 mt-1 text-2xl font-semibold"
+            >
+              Volver a la tienda
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
