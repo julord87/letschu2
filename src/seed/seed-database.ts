1d5b34884c5c73cb09ee793b9b57c98f20b6d5a9
Julian Martinez Gomez <julianmzg@gmail.com>
2024-09-12 08:40:40 -0300

    feat: Add Prisma client singleton and global instance
    This commit adds a new file, prisma.ts, which exports a Prisma client singleton. The singleton pattern ensures that only one instance of the Prisma client is created and reused throughout the application. Additionally, the globalThis object is used to store the Prisma client instance, allowing for easy access and sharing across different modules. This change improves the efficiency and performance of database operations.
    
    Note: This commit message follows the established conventions in the repository.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,15 +1,18 @@
 import { initialData } from "./seed";
-
+import prisma from "../lib/prisma";
 
 
 async function main() {
+    await Promise.all([
+        await prisma.productImage.deleteMany(),
+        await prisma.product.deleteMany(),
+        await prisma.type.deleteMany()
+    ])
+    
 
 
 
-    console.log(initialData)
-
-
-    console.log('seed-database');
+    console.log('seed-database ejecutado');
 }
 
 
