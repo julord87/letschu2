5f018c86b6ed52c24ea35ea0e0fb16c9c2715184
Julian Martinez Gomez <julianmzg@gmail.com>
2024-10-11 11:32:50 -0300

    Refactor AddToCart component in product page and fix ESLint errors

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
@@ -6,26 +6,40 @@ import { ColorSelector, QuantitySelector } from "@/components";
 import { Colors, Product } from "@/interfaces";
 
 interface Props {
-    product: Product;
+  product: Product;
 }
 
 export const AddToCart = ({ product }: Props) => {
+  const [color, setColor] = useState<Colors | undefined>();
+  const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState(false);
 
-    const [color, setColor] = useState<Colors|undefined>();
-    const [quantity, setQuantity] = useState<number>(1);
+  const addToCart = () => {
+    setPosted(true);
 
+    if (!color || !quantity) return;
 
+    console.log({ color, quantity });
+  };
 
   return (
     <>
+      {" "}
+      {posted && !color && (
+        <span className="mt-2 text-red-500 fade-in">Selecciona un color*</span>
+      )}
       {/* Select color */}
-      <ColorSelector availableColors={product.colors} selectedColor={color} onColorChange={setColor} />
-
+      <ColorSelector
+        availableColors={product.colors}
+        selectedColor={color}
+        onColorChange={setColor}
+      />
       {/* Select quantity */}
       <QuantitySelector quantity={quantity} onQuantityChange={setQuantity} />
-
       {/* Add to cart Button */}
-      <button className="btn-primary my-5">Agregar al carrito</button>
+      <button onClick={addToCart} className="btn-primary my-5">
+        Agregar al carrito
+      </button>
     </>
   );
 };
