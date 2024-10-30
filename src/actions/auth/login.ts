3cac0f95d9c76bfd027b10cb6b2eea5a27631484
Julian Martinez Gomez <julianmzg@gmail.com>
2024-10-30 09:01:34 -0300

    Refactor authentication configuration and add login functionality

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -30,3 +30,21 @@ export async function authenticate(
     return 'Unknow Error';
   }
 }
+
+export const login = async(email: string, password: string) => {
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+    })
+
+    return {ok: true}
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: "Error al iniciar sesión"
+    }
+  }
+}
