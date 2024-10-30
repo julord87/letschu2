32953597571f19fd1f1f4c18986253a22d77fd17
Julian Martinez Gomez <julianmzg@gmail.com>
2024-10-30 08:19:49 -0300

    Refactor authentication configuration and add react-hook-form dependency

---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import clsx from "clsx";
+import { errorToJSON } from "next/dist/server/render";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormInputs = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    const { name, email, password } = data;
+    console.log({ name, email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+      {
+        /* mensajes de error */
+        // errors.name?.type === "required" && (
+        //   <p className="text-red-500 text-sm text-center">
+        //     El nombre es obligatorio
+        //   </p>
+        // )
+
+        /* mensajes de exito */
+      }
+
+      <label htmlFor="name">Nombre</label>
+      <input
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.name,
+        })}
+        type="text"
+        {...register("name", { required: true })}
+        autoFocus
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.email,
+        })}
+        type="email"
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+
+      <label htmlFor="password">Contraseña</label>
+      <input
+        className={clsx("px-5 py-2 border bg-gray-200 rounded mb-5", {
+          "border-red-500": errors.password,
+        })}
+        type="password"
+        {...register("password", { required: true, minLength: 6 })}
+      />
+
+      <button className="btn-primary">Crear cuenta</button>
+
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link href="/auth/login" className="btn-secondary text-center">
+        Ingresar
+      </Link>
+    </form>
+  );
+};
