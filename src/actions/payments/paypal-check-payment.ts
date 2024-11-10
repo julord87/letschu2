7fb3969cf7145e7cc5ddf803ee5e55ad631d058a
Julian Martinez Gomez <julianmzg@gmail.com>
2024-11-10 19:49:31 -0300

    Add PayPal payment verification and response handling; introduce PayPalOrderStatusResponse interface

--- src/actions/payments/paypal-check-payment.ts
+++ src/actions/payments/paypal-check-payment.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { PayPalOrderStatusResponse } from "@/interfaces";
+
 
 
 export const payPalCheckPayment = async ( payPalTransactionId: string) => {
@@ -12,6 +14,26 @@ export const payPalCheckPayment = async ( payPalTransactionId: string) => {
             message: "Error al obtener el token de autenticación"
         }
     }
+
+    const resp = await verifyPayPalPayment(payPalTransactionId, authTOken);
+
+    if( !resp ) {
+        return {
+            ok: false,
+            message: "Error al verificar el pago con PayPal"
+        }
+    }
+
+    const { status, purchase_units } = resp;
+    // const {} = purchase_units[0]
+
+    if( status !== "COMPLETED" ) {
+        return {
+            ok: false,
+            message: "El pago no fue completado"
+        }
+    }
+
 }
 
 const getPayPalBearerToken = async (): Promise<string | null> => {
@@ -37,6 +59,29 @@ const getPayPalBearerToken = async (): Promise<string | null> => {
     try {
         const response = await fetch(`${process.env.PAYPAL_OAUTH_URL}`, requestOptions).then((res) => res.json());
         return response.access_token;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+const verifyPayPalPayment = async (
+    payPalTransactionId: string, 
+    bearerToken: string
+    ): Promise<PayPalOrderStatusResponse | null> => {
+    const payPalOrderUrl = `${process.env.PAYPAL_ORDERS_URL}/${payPalTransactionId}`;
+    
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${bearerToken}`);
+    
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+    
+    try {
+        const response = await fetch(payPalOrderUrl, requestOptions).then((res) => res.json());
+        return response;
     } catch (error) {
         console.error(error);
         return null;
