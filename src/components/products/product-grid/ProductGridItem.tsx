e3afa047defdf46e933dff568e1d118844b9208e
Julian Martinez Gomez <julianmzg@gmail.com>
2024-12-12 10:10:30 -0300

    Increase pagination limit for product retrieval and enhance image display logic in ProductGridItem component

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,8 +9,17 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
 
-  const [displayImage, setDisplayImage] = useState( product.images[0] );
+  const handleMouseEnter = () => {
+    if (product.images[1]) {
+      setDisplayImage(product.images[1]);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setDisplayImage(product.images[0]);
+  };
 
   return (
     <div className="rounded-md overflow-hidden fade-in">
@@ -21,20 +30,20 @@ export const ProductGridItem = ({ product }: Props) => {
           className="w-full object-cover rounded"
           width={500}
           height={500}
-          onMouseEnter={() => setDisplayImage(product.images[1])}
-          onMouseLeave={() => setDisplayImage(product.images[0])}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         />
       </Link>
 
       <div className="p-4 flex flex-col">
         <Link
-          className="hover:text-blue-600 transition-all" 
-          href={`/product/${product.slug}`}>
+          className="hover:text-blue-600 transition-all"
+          href={`/product/${product.slug}`}
+        >
           {product.title}
         </Link>
         <span className="font-bold">${product.price}</span>
       </div>
-
     </div>
   );
 };
