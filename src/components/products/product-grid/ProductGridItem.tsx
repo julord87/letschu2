292a59aee1479a0ccd7154dcfead9da63f6db79c
Julian Martinez Gomez <julianmzg@gmail.com>
2024-12-12 10:20:08 -0300

    Refactor ProductGridItem component to simplify image display logic and improve readability

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,17 +9,8 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
-  const [displayImage, setDisplayImage] = useState(product.images[0]);
 
-  const handleMouseEnter = () => {
-    if (product.images[1]) {
-      setDisplayImage(product.images[1]);
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setDisplayImage(product.images[0]);
-  };
+  const [displayImage, setDisplayImage] = useState( product.images[0] );
 
   return (
     <div className="rounded-md overflow-hidden fade-in">
@@ -30,20 +21,20 @@ export const ProductGridItem = ({ product }: Props) => {
           className="w-full object-cover rounded"
           width={500}
           height={500}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
         />
       </Link>
 
       <div className="p-4 flex flex-col">
         <Link
-          className="hover:text-blue-600 transition-all"
-          href={`/product/${product.slug}`}
-        >
+          className="hover:text-blue-600 transition-all" 
+          href={`/product/${product.slug}`}>
           {product.title}
         </Link>
         <span className="font-bold">${product.price}</span>
       </div>
+
     </div>
   );
 };
