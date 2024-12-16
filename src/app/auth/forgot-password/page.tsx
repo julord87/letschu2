980ead891c15bdaa37905ac7ef02b6a0919d4c08
Julian Martinez <julianmzg@gmail.com>
2024-12-16 10:45:06 -0300

    Update page.tsx

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import { ForgotPasswordForm } from './ui/ForgotPasswordForm';
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+    <div className="flex flex-col min-h-screen pt-16 sm:pt-32">
 
       <div className="px-5 mx-5">
         <Image
