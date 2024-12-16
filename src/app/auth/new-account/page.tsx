eeeb0ef0d10f71a28d7febfa61cd9d2dfd55298f
Julian Martinez <julianmzg@gmail.com>
2024-12-16 10:45:18 -0300

    Update page.tsx

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -5,7 +5,7 @@ import { RegisterForm } from './ui/RegisterForm';
 
 export default function NewAccountPage() {
   return (
-    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+    <div className="flex flex-col min-h-screen pt-16 sm:pt-32">
 
       <div className="px-5 mx-5">
         <Image
